77b8013c8cb068c571d08b3dced51018f9a62af2
wes hatch <wes.hatch@gmail.com>
2023-10-29 16:39:21 -0400

    modernizing server

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 var express = require('express');
 var fs = require('fs')
 // var path = require('path');
+
 var port = 3000;
 var app = express();
+var thePossibilites = [];
+
+fs.readdirSync('public').forEach(function(name) {
+  route = 'public/' + name + '/index.html';
+  if (fs.existsSync(route)) {
+    thePossibilites.push(route);
+  }
+});
 
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res){
-	var thePossibilites = [];
-	var route;
-	var random;
+app.use(function(req, res) {
+  var site = (req.query.site && req.query.site < thePossibilites.length)
+    ? req.query.site
+    : Math.floor(Math.random() * thePossibilites.length);
+  // site = 2;  // not so random. Rain
+  // site = 4;  // not so random. Waves
 
-	fs.readdirSync('public').forEach( function(name) {
-		route = 'public/' + name + '/index.html';
-		if (fs.existsSync(route)) {
-			thePossibilites.push(route);
-		}
-	});
-
-	random = Math.floor(Math.random() * thePossibilites.length);
-	// random = 0;// not so random
-
-	res.sendfile( thePossibilites[random] );
+  res.sendfile( thePossibilites[site] );
 });
-
-
 
 app.listen(port, function() {
   console.log('malfunction at http://localhost:' + port);
 });
-
-// module.exports = app;
