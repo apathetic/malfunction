4996d1fa14e7f4230d1f87075520871c181cf68d
wes hatch <wes.hatch@gmail.com>
2023-10-29 16:30:45 -0400

    adding portuguese stuffs

---
+++ public/portuguese/irreg.php
@@ -0,0 +1,514 @@
+<?php
+
+  switch($_REQUEST["action"]) {
+    case 'present':
+      print('present|
+  <span class="tt">irregular</span> <a href="javascript:hideSection(\'present\')">(hide &gt;&gt;)</a><br />
+
+  <ul class="ind">
+    <ul class="horiz">
+      <li><b>dar</b></li>
+      <li>dou</li>
+      <li>d&aacute;s</li>
+      <li>d&aacute;</li>
+      <li>damos</li>
+      <li>d&atilde;o</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>estar</b></li>
+      <li>estou</li>
+      <li>est&aacute;s</li>
+      <li>est&aacute;</li>
+      <li>estamos</li>
+      <li>est&atilde;o</li>
+    </ul>
+    
+    <ul class="horiz">
+      <li><b>dizer</b></li>
+      <li>digo</li>
+      <li>dizes</li>
+      <li>diz</li>
+      <li>dizemos</li>
+      <li>dizem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>fazer</b></li>
+      <li>fa&ccid;o</li>
+      <li>fazes</li>
+      <li>faz</li>
+      <li>fazemos</li>
+      <li>fazem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>trazer</b></li>
+      <li>trago</li>
+      <li>trazes</li>
+      <li>traz</li>
+      <li>trazemos</li>
+      <li>trazem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>haver</b></li>
+      <li>hei</li>
+      <li>h&aacute;s</li>
+      <li>h&aacute;</li>
+      <li>havemos</li>
+      <li>h&atilde;o</li>
+   </ul>
+
+   <ul class="horiz">
+      <li><b>ler</b></li>
+      <li>leio</li>
+      <li>l&ecirc;s</li>
+      <li>l&ecirc;</li>
+      <li>lemos</li>
+      <li>l&ecirc;em</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ver</b></li>
+      <li>veio</li>
+      <li>v&ecirc;s</li>
+      <li>v&ecirc;</li>
+      <li>vemos</li>
+      <li>v&ecirc;em</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>perder</b></li>
+      <li>perco</li>
+      <li>perdes</li>
+      <li>perde</li>
+      <li>perdemos</li>
+      <li>perdem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>poder</b></li>
+      <li>posso</li>
+      <li>podes</li>
+      <li>pode</li>
+      <li>podemos</li>
+      <li>podem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>querer</b></li>
+      <li>quero</li>
+      <li>queres</li>
+      <li>quer</li>
+      <li>queremos</li>
+      <li>querem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>saber</b></li>
+      <li>sei</li>
+      <li>sabes</li>
+      <li>sabe</li>
+      <li>sabemos</li>
+      <li>sabem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ser</b></li>
+      <li>sou</li>
+      <li>&eacute;s</li>
+      <li>&eacute;</li>
+      <li>somos</li>
+      <li>s&atilde;o</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ter</b></li>
+      <li>tenho</li>
+      <li>tens</li>
+      <li>tem</li>
+      <li>temos</li>
+      <li>t&eacute;m</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>vir</b></li>
+      <li>venho</li>
+      <li>vens</li>
+      <li>vem</li>
+      <li>vimos</li>
+      <li>v&eacute;m</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>dormir</b></li>
+      <li>durmo</li>
+      <li>dormes</li>
+      <li>dorme</li>
+      <li>dormimos</li>
+      <li>dormem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ir</b></li>
+      <li>vou</li>
+      <li>vais</li>
+      <li>vai</li>
+      <li>vamos</li>
+      <li>v&atilde;o</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ouvir</b></li>
+      <li>ou&ccidil;o/oi&ccidil;o</li>
+      <li>ouves</li>
+      <li>ouve</li>
+      <li>ouvimos</li>
+      <li>ouvem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>pedir</b></li>
+      <li>pe&ccedil;o</li>
+      <li>pedes</li>
+      <li>pede</li>
+      <li>pedimos</li>
+      <li>pedem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>sair</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>servir</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>subir</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>p&ocirc;r</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+  </ul>');
+      break;
+
+
+    case 'past':
+      print('past|
+  <span class="tt">irregular</span> <a href="javascript:hideSection(\'past\')">(hide &gt;&gt;)</a><br />
+
+  <ul class="ind">
+    <ul class="horiz">
+      <li><b>dar</b></li>
+      <li>dou</li>
+      <li>d&aacute;s</li>
+      <li>d&aacute;</li>
+      <li>damos</li>
+      <li>d&atilde;o</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>estar</b></li>
+      <li>estou</li>
+      <li>est&aacute;s</li>
+      <li>est&aacute;</li>
+      <li>estamos</li>
+      <li>est&atilde;o</li>
+    </ul>
+    
+    <ul class="horiz">
+      <li><b>dizer</b></li>
+      <li>digo</li>
+      <li>dizes</li>
+      <li>diz</li>
+      <li>dizemos</li>
+      <li>dizem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>fazer</b></li>
+      <li>fa&ccid;o</li>
+      <li>fazes</li>
+      <li>faz</li>
+      <li>fazemos</li>
+      <li>fazem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>trazer</b></li>
+      <li>trago</li>
+      <li>trazes</li>
+      <li>traz</li>
+      <li>trazemos</li>
+      <li>trazem</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>haver</b></li>
+      <li>hei</li>
+      <li>h&aacute;s</li>
+      <li>h&aacute;</li>
+      <li>havemos</li>
+      <li>h&atilde;o</li>
+   </ul>
+
+   <ul class="horiz">
+      <li><b>ler</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ver</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>perder</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>poder</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>querer</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>saber</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ser</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ter</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>vir</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ir</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>ouvir</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>sair</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+
+   <ul class="horiz">
+      <li><b>p&ocirc;r</b></li>
+      <li>more</li>
+      <li>verbs</li>
+      <li>to</li>
+      <li>put</li>
+      <li>here</li>
+    </ul>
+   </ul>');
+      break;
+
+
+    case 'future':
+      print('future|
+  <span class="tt">irregular</span> <a href="javascript:hideSection(\'future\')">(hide &gt;&gt;)</a><br />
+
+  <ul class="ind">
+    <ul class="horiz">
+      <li><b>dizer</b></li>
+      <li>direi</li>
+      <li>dir&aacute;s</li>
+      <li>dir&aacute;</li>
+      <li>diremos</li>
+      <li>dir&atilde;o</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>fazer</b></li>
+      <li>farei</li>
+      <li>far&aacute;s</li>
+      <li>far&aacute;</li>
+      <li>faremos</li>
+      <li>far&atilde;o</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>trazer</b></li>
+      <li>trarei</li>
+      <li>trar&aacute;s</li>
+      <li>trar&aacute;</li>
+      <li>traremos</li>
+      <li>trar&atilde;o</li>
+    </ul>
+  </ul>');
+      break;
+
+
+    case 'imp':
+      print('imp|
+  <span class="tt">irregular</span> <a href="javascript:hideSection(\'imp\')">(hide &gt;&gt;)</a><br />
+  <ul class="ind">
+    <ul class="horiz">
+      <li><b>ser</b></li>
+      <li>era</li>
+      <li>eras</li>
+      <li>era</li>
+      <li>&eacute;ramos</li>
+      <li>eram</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>ter</b></li>
+      <li>tinha</li>
+      <li>tinhas</li>
+      <li>tinha</li>
+      <li>t&iacute;nhamos</li>
+      <li>tinham</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>vir</b></li>
+      <li>vinha</li>
+      <li>vinhas</li>
+      <li>vinha</li>
+      <li>v&iacute;nhamos</li>
+      <li>vinham</li>
+    </ul>
+
+    <ul class="horiz">
+      <li><b>p&ocirc;r</b></li>
+      <li>punha</li>
+      <li>punhas</li>
+      <li>punha</li>
+      <li>p&uacute;nhamos</li>
+      <li>punham</li>
+    </ul>
+  </ul>');
+      break;
+
+    case 'cond':
+      print('cond|
+  <span class="tt">irregular</span> <a href="javascript:hideSection(\'cond\')">(hide &gt;&gt;)</a><br />
+
+  <ul class="ind">
+    <ul class="horiz">
+      <li><b>dizer</b></li>
+      <li>diria</li>
+      <li>dirias</li>
+      <li>diria</li>
+      <li>dir&iacute;amos</li>
+      <li>diriam</li>
+    </ul>
+    <ul class="horiz">
+      <li><b>fazer</b></li>
+      <li>faria</li>
+      <li>farias</li>
+      <li>faria</li>
+      <li>far&iacute;amos</li>
+      <li>fariam</li>
+    </ul>
+    <ul class="horiz">
+      <li><b>trazer</b></li>
+      <li>traria</li>
+      <li>trarias</li>
+      <li>traria</li>
+      <li>trar&iacute;amos</li>
+      <li>trariam</li>
+    </ul>
+  </ul>');
+      break;
+
+    default:
+      print("error");
+}
+
+return true;
+
+?>
