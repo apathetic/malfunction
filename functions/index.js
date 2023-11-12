fa2d07b0a9a394f7b1b6ba82755729dad5d4ed31
wes hatch <wes.hatch@gmail.com>
2023-11-11 23:38:49 -0500

    i dont know

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,38 @@
 import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler';
-import manifestJSON from '__STATIC_CONTENT_MANIFEST';
 
-const assetManifest = JSON.parse(manifestJSON);
-const apps = Object.keys(assetManifest).filter((ass) => ass.match(/index.html/));
+
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON || '{}');
+// const apps = Object.keys(assetManifest).filter((ass) => ass.match(/index.html/));
+
+const apps = ['ascii', 'birds', 'lavaflow', 'portuguese', 'ragdolls', 'rain', 'ryoiji', 'starmap', 'synth', 'waves'];
 
 
 export async function onRequest(context) {
     const { request, env, waitUntil } = context;
-    const url = new URL(request.url);
-    const options = {
-      ASSET_NAMESPACE: env.__STATIC_CONTENT,
-      ASSET_MANIFEST: assetManifest,
-    };
+    const time = new Date().toISOString();
+
+    // const assetManifest = JSON.parse(env.__STATIC_CONTENT_MANIFEST);
+    // const apps = Object.keys(assetManifest).filter((ass) => ass.match(/index.html/));
+    // const task = await context.env.FILES.get('ascii/ascii.4dabe0506f.html');
+    // console.log(context.env);
+  //   const task = await context.env.ASSETS.get('ascii/index.html');
+  //   console.log(task);
+  // return new Response(task);
+
+
 
     const app = apps[~~(Math.random() * apps.length)];
-    console.log('CONGRATS YOU\'RE GETTING:', app);
+    console.log(`[${time}] CONGRATS YOU'RE GETTING: ${app}`);
 
-    url.pathname = app;
-    options.mapRequestToAsset = (req) => mapRequestToAsset(new Request(url, req));
+    const url = new URL(request.url);
+    url.pathname = app + '/index.html';
+
+    const options = {
+      ASSET_NAMESPACE: env.__STATIC_CONTENT,
+      ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST, // assetManifest,
+      mapRequestToAsset: (req) => mapRequestToAsset(new Request(url, req))
+    };
 
     try {
       return await getAssetFromKV({ request, waitUntil }, options);
