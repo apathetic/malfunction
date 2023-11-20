76d33b5898dfb6997c87464f48e200321af0e388
wes hatch <wes.hatch@gmail.com>
2023-11-19 22:17:59 -0500

    do you see HOW SHIT YOUR DX IS CLOUDFLARE??

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,22 +1,30 @@
-const apps = ['ascii/', 'birds/', 'lavaflow/', 'portuguese/', 'ragdolls/', 'rain/', 'ryoiji/', 'starmap/', 'synth/', 'waves/'];
+const apps = ['ascii/', 'birds/', 'lavaflow/', 'portuguese/', 'ragdolls/', 'rain/', 'ryoji/', 'starmap/', 'synth/', 'waves/'];
 
+/**
+ * This is all terrible i don't care really it's cloudflare's fault for
+ * such a shit API and DX and even MORE terrible docs I couldn't figure
+ * out a better way to _NOT_ rewrite EVERY. SINGLE. ASSET. REQUEST. but
+ * oh well it is what it is
+ * @param {*} param0
+ * @returns
+ */
 export async function onRequest({ request, env, params }) {
+  const url = new URL(request.url);
   const pathSegments = params.catchall;
   console.log('ATCHALl', JSON.stringify(pathSegments));
 
   if (!pathSegments) {
     const time = new Date().toISOString();
-    const url = new URL(request.url);
     const app = apps[~~(Math.random() * apps.length)];
     console.log(`[${time}] CONGRATS YOU'RE GETTING: ${app}`);
-
     url.pathname = '/public/' + app;
-    const randomApp = new Request(url, request)
-
-    console.log(request.url, randomApp.url);
-    return env.ASSETS.fetch(randomApp);
+  } else {
+    url.pathname = '/public/' + pathSegments.join('/'); // request.url.pathname;
   }
 
-  return env.ASSETS.fetch(request);
+  const whoCares = new Request(url, request)
+  console.log(request.url, whoCares.url);
+
+  return env.ASSETS.fetch(whoCares);
 }
 
