49a572c2bbce982615074dbee406c2f67626236e
wes hatch <wes.hatch@gmail.com>
2023-11-19 22:10:03 -0500

    do you see HOW SHIT YOUR DX IS CLOUDFLARE??

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,22 +1,22 @@
 const apps = ['ascii/', 'birds/', 'lavaflow/', 'portuguese/', 'ragdolls/', 'rain/', 'ryoiji/', 'starmap/', 'synth/', 'waves/'];
 
-export async function onRequest({ request, env, params, waitUntil }) {
+export async function onRequest({ request, env, params }) {
+  const pathSegments = params.catchall;
+  console.log('ATCHALl', JSON.stringify(pathSegments));
 
-  const time = new Date().toISOString();
-  const url = new URL(request.url);
-  const app = apps[~~(Math.random() * apps.length)];
-  // console.log(`[${time}] CONGRATS YOU'RE GETTING: ${app}`);
+  if (!pathSegments) {
+    const time = new Date().toISOString();
+    const url = new URL(request.url);
+    const app = apps[~~(Math.random() * apps.length)];
+    console.log(`[${time}] CONGRATS YOU'RE GETTING: ${app}`);
 
-  console.log('ATCHALl', JSON.stringify(params.catchall));
+    url.pathname = '/public/' + app;
+    const randomApp = new Request(url, request)
 
-  url.pathname = '/public/' + app;
-  const newRequest = new Request(url, request)
+    console.log(request.url, randomApp.url);
+    return env.ASSETS.fetch(randomApp);
+  }
 
-  console.log(request.url, newRequest.url);
-
-
-  // const x = await env.ASSETS.fetch(newRequest);
-  return env.ASSETS.fetch(newRequest);
-
+  return env.ASSETS.fetch(request);
 }
 
